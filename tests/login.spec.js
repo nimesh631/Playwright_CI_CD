377c12ccb3729725e8891ab1090779a0c970d407
nimesh631 <nimeshkayastha067@gmail.com>
2026-06-07 12:51:36 +0545

    login test script

---
+++ tests/login.spec.js
@@ -0,0 +1,7 @@
+const { test, expect } = require('@playwright/test');
+
+test('login page loads', async ({ page }) => {
+  await page.goto('https://example.com');
+
+  await expect(page).toHaveTitle(/Example/);
+});
