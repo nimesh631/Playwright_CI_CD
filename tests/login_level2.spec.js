d4c65afef1b1130215ab34fbe9331065fe3d0609
nimesh631 <nimeshkayastha067@gmail.com>
2026-06-07 13:25:25 +0545

    add login flow test

---
+++ tests/login_level2.spec.js
@@ -0,0 +1,19 @@
+const { test, expect } = require('@playwright/test');
+
+test('login flow simulation', async ({ page }) => {
+  
+  // go to a demo login page
+  await page.goto('https://the-internet.herokuapp.com/login');
+
+  // fill username
+  await page.fill('#username', 'tomsmith');
+
+  // fill password
+  await page.fill('#password', 'SuperSecretPassword!');
+
+  // click login
+  await page.click('button[type="submit"]');
+
+  // assertion
+  await expect(page.locator('#flash')).toContainText('You logged into a secure area!');
+});
